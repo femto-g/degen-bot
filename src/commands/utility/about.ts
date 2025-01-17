61a70fa38f374cc4327e052b121a0fbc35c669f8
femto-g <121134764+femto-g@users.noreply.github.com>
2025-01-17 12:44:06 -0500

    Updated about reply

--- src/commands/utility/about.ts
+++ src/commands/utility/about.ts
@@ -7,11 +7,14 @@ export const data = new SlashCommandBuilder()
 export async function execute(interaction: any) {
   const aboutMessage = `## Degenbot: A simple bot for market data
 ### Commands:
-/about: displays information about the bot **<-- you are here**
-/snapshot : View a snapshot of a particular asset
-/info : similar to snapshot but additionally provides an ohlc/sma chart 
+- **/about**: Displays information about the bot **<-- you are here**
+- **/snapshot**: View a snapshot of a particular asset
+- **/info** : Similar to snapshot but additionally provides an ohlc/sma chart 
+- **/list** : Displays the snapshots of currently tracked assets
+- **/track** : Add an asset to the tracking list
+- **/untrack** : Remove an asset from the tracking list
 ### Tips:
-- When using /snapshot or /info for a crypto asset, make sure you are using a valid ticker; for example, the ticker symbol to track the USD price of bitcoin is not 'BTC' but rather, 'BTCUSD'.
+- When using providing a ticker symbol for a command, make sure you are using a valid ticker (e.g. for the USD price of Bitcoin, the symbol is ~~BTC~~ BTCUSD)
 ### Caveats:
 - This bot uses the Stocks Basic tier of the [polygon.io](<https://polygon.io/>) api and has the corresponding limitations. This means it is rate limited to a handful of requests PER minute. This also means that daily market date is only available AFTER the end of the market day. Any daily data viewed before then will be delayed.
 -# See [polygon.io/pricing](<https://polygon.io/pricing>) for more information`;
